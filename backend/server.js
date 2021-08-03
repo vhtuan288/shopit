99b2b817617ca5953f38b17fad8dbff00a30d0d4
tuanhuyvu <tuanhuy.vu@isobar.com>
2021-08-03 18:54:24 +0700

    fixed dotenv

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ process.on('uncaughtException', err => {
 
 // Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION') {
-    require('dotenv').dotenv.config({path: 'backend/config/config.env'});
+    require('dotenv').config({path: 'backend/config/config.env'});
 }
 
 // Connecting to Database
